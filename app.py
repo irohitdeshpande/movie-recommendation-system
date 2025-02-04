91a843acf699a324d9ecc79c04a6f3a3c890a149
rohit deshpande <150110009+rohitd0818@users.noreply.github.com>
2025-02-04 22:56:36 +0530

    Update app.py, movie_dict.pkl, ContentBasedFiltering.ipynb, similarity.pkl files

--- app.py
+++ app.py
@@ -3,21 +3,13 @@ import pickle
 import pandas as pd
 import requests
 import numpy as np
-from tensorflow.keras.models import load_model
 
 # Load data and models
 def load_data():
     # Load content-based filtering data
-    movies_dict = pickle.load(open('pickle_files/movie_dict.pkl', 'rb'))
+    movies_dict = pickle.load(open('movie_dict.pkl', 'rb'))
     movies = pd.DataFrame(movies_dict)
-    similarity = pickle.load(open('pickle_files/similarity.pkl', 'rb'))
-
-    # # Load collaborative filtering data
-    # user_id_map = pickle.load(open('pickle_files/user_id_map.pkl', 'rb'))
-    # movie_id_map = pickle.load(open('pickle_files/movie_id_map.pkl', 'rb'))
-    # movies_cf = pickle.load(open('pickle_files/movies.pkl', 'rb'))
-    # model = load_model('pickle_files/deep_learning_model.keras')
-
+    similarity = pickle.load(open('similarity.pkl', 'rb'))
     return movies, similarity
 
 # Fetch movie poster
@@ -41,39 +33,10 @@ def content_based_recommend(movie, movies, similarity, n=5):
         recommended_movie_names.append(movies.iloc[i[0]].title)
     return recommended_movie_names, recommended_movie_posters
 
-# # Collaborative filtering recommendation
-# def collaborative_filtering_recommend(user_id, movies_cf, model, user_id_map, movie_id_map, n=5):
-#     user_index = user_id_map.get(user_id, None)
-#     if user_index is None:
-#         return [], []  # If user ID is not found, return empty lists
-    
-#     movie_indices = np.arange(len(movie_id_map))
-#     user_indices = np.full(len(movie_id_map), user_index)
 
-#     predictions = model.predict([user_indices, movie_indices]).flatten()
-#     top_n_indices = predictions.argsort()[-n:][::-1]  # Get top N recommendations
-#     top_movie_ids = [list(movie_id_map.keys())[i] for i in top_n_indices]  # Convert back to actual movie IDs
-    
-#     # Get movie titles and posters
-#     recommended_movie_names = []
-#     recommended_movie_posters = []
-#     for movie_id in top_movie_ids:
-#         movie_title = movies_cf[movies_cf['id'] == movie_id]['title'].values[0]
-#         recommended_movie_names.append(movie_title)
-#         recommended_movie_posters.append(fetch_poster(movie_id))
-    
-#     return recommended_movie_names, recommended_movie_posters
-
-# Hybrid recommendation function
-def hybrid_recommend(movie_title, movies, similarity, n=5):
+def recommend(movie_title, movies, similarity, n=5):
     # Content-based recommendations
     cb_names, cb_posters = content_based_recommend(movie_title, movies, similarity, n)
-    
-    # # Collaborative filtering recommendations (if user_id is not provided, use a default user_id)
-    # default_user_id = 1  # You can change this to any default user ID
-    # cf_names, cf_posters = collaborative_filtering_recommend(default_user_id, movies_cf, model, user_id_map, movie_id_map, n)
-    
-    # Combine and deduplicate recommendations
     all_names = cb_names
     all_posters = cb_posters 
     unique_names, unique_posters = [], []
@@ -121,7 +84,7 @@ def main():
             st.error("Please select a genre or a movie.")
 
         # Get hybrid recommendations
-        recommended_names, recommended_posters = hybrid_recommend(
+        recommended_names, recommended_posters = recommend(
             selected_movie, movies, similarity, n=5
         )
 
